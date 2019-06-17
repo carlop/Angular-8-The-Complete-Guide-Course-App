033b400a04bee105a12a5052c9a57210f887e38e
Carlos López <yo@carlop.es>
2019-06-17 19:11:51 +0200

    Adding content to the Recipes component

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe 1', 'This is a simply test', 'https://www.unapizza.es/sites/default/files/styles/receta_full/public/pizza-funghi.jpg'),
+    new Recipe('A Test Recipe 2', 'This is a simply test', 'https://www.unapizza.es/sites/default/files/styles/receta_full/public/pizza-funghi.jpg'),
+    new Recipe('A Test Recipe 3', 'This is a simply test', 'https://www.unapizza.es/sites/default/files/styles/receta_full/public/pizza-funghi.jpg'),
+    new Recipe('A Test Recipe 4', 'This is a simply test', 'https://www.unapizza.es/sites/default/files/styles/receta_full/public/pizza-funghi.jpg')
+  ];
 
   constructor() { }
 
