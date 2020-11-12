97b784ddbd9b62305f7782bb7ec810528942dcbb
Carlos López <yo@carlop.es>
2020-11-12 19:40:20 +0100

    Adds ingredients to shopping list from recipe description.

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,9 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
@@ -44,7 +46,14 @@ export class RecipeService {
         new Ingredient('Oregano', 1)
       ])
   ];
+
+  constructor(private shoppingList: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingList.addIngredients(ingredients);
   }
 }
