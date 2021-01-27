a3891b72f2ae5c8acb524c286b2f29deaa523fa8
Carlos López <yo@carlop.es>
2021-01-27 13:31:39 +0100

    Preparing the signup request

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface AuthResponseData {
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+  // No problem with showing API_KEY: https://stackoverflow.com/a/37484053
+  API_KEY = 'AIzaSyDwXF-KLPZ0Nk7TAliH5TBxDEScTafIGGg';
+
+  constructor(private http: HttpClient) {
+  }
+
+  signup(email: string, password: string) {
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + this.API_KEY,
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      });
+  }
+}
