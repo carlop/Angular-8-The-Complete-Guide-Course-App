a852b63ef2553e1ff081ad4104b5253fe6037d38
Carlos López <yo@carlop.es>
2021-02-01 12:48:52 +0100

    Wrap up

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -67,81 +67,81 @@ export class AuthService {
               responseData.localId,
               responseData.idToken,
               +responseData.expiresIn
-            );
-          }
-        )
+              );
+            }
+          )
+        );
+    }
+
+    autoLogin() {
+      const userData: {
+        email: string;
+        id: string;
+        _token: string;
+        _tokenExpirationDate: Date;
+      } = JSON.parse(localStorage.getItem('userData'));
+      if (!this.user) {
+        return;
+      }
+
+      const loadedUser = new User(
+        userData.email,
+        userData.id,
+        userData._token,
+        new Date(userData._tokenExpirationDate)
       );
-  }
 
-  autoLogin() {
-    const userData: {
-      email: string;
-      id: string;
-      _token: string;
-      _tokenExpirationDate: Date;
-    } = JSON.parse(localStorage.getItem('userData'));
-    if (!this.user) {
-      return;
+      if (loadedUser.token) {
+        this.user.next(loadedUser);
+        const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+        this.autoLogout(expirationDuration);
+      }
     }
 
-    const loadedUser = new User(
-      userData.email,
-      userData.id,
-      userData._token,
-      new Date(userData._tokenExpirationDate)
-    );
-
-    if (loadedUser.token) {
-      this.user.next(loadedUser);
-      const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
-      this.autoLogout(expirationDuration);
+    logout() {
+      this.user.next(null);
+      this.router.navigate(['/auth']);
+      localStorage.removeItem('userData');
+      if (this.tokenExpirationTimer) {
+        clearTimeout(this.tokenExpirationTimer);
+      }
+      this.tokenExpirationTimer = null;
     }
-  }
 
-  logout() {
-    this.user.next(null);
-    this.router.navigate(['/auth']);
-    localStorage.removeItem('userData');
-    if (this.tokenExpirationTimer) {
-      clearTimeout(this.tokenExpirationTimer);
+    autoLogout(expirationDuration: number) {
+      console.log(expirationDuration);
+      this.tokenExpirationTimer = setTimeout(() => {
+        this.logout();
+      }, expirationDuration);
     }
-    this.tokenExpirationTimer = null;
-  }
 
-  autoLogout(expirationDuration: number) {
-    console.log(expirationDuration);
-    this.tokenExpirationTimer = setTimeout(() => {
-      this.logout();
-    }, expirationDuration);
-  }
+    private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
+      const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+      const user = new User(email, userId, token, expirationDate);
+      this.user.next(user);
+      this.autoLogout(expiresIn * 1000);
+      localStorage.setItem('userData', JSON.stringify(user));
+    }
 
-  private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
-    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
-    const user = new User(email, userId, token, expirationDate);
-    this.user.next(user);
-    this.autoLogout(expiresIn * 1000);
-    localStorage.setItem('userData', JSON.stringify(user));
-  }
-
-  private handleError(errorResponse: HttpErrorResponse) {
-    let errorMessage = 'An unknown error occurred!';
-    if (!errorResponse.error || !errorResponse.error.error) {
+    private handleError(errorResponse: HttpErrorResponse) {
+      let errorMessage = 'An unknown error occurred!';
+      if (!errorResponse.error || !errorResponse.error.error) {
+        return throwError(errorMessage);
+      }
+      switch (errorResponse.error.error.message) {
+        case 'EMAIL_EXISTS':
+          errorMessage = 'This email exists already!';
+          break;
+        case 'EMAIL_NOT_FOUND':
+          errorMessage = 'This email was not found!';
+          break;
+        case 'INVALID_PASSWORD':
+          errorMessage = 'The password is not correct!';
+          break;
+        case 'USER_DISABLED':
+          errorMessage = 'This user has been disabled!';
+          break;
+      }
       return throwError(errorMessage);
     }
-    switch (errorResponse.error.error.message) {
-      case 'EMAIL_EXISTS':
-        errorMessage = 'This email exists already!';
-        break;
-      case 'EMAIL_NOT_FOUND':
-        errorMessage = 'This email was not found!';
-        break;
-      case 'INVALID_PASSWORD':
-        errorMessage = 'The password is not correct!';
-        break;
-      case 'USER_DISABLED':
-        errorMessage = 'This user has been disabled!';
-        break;
-    }
-    return throwError(errorMessage);
   }
-}
