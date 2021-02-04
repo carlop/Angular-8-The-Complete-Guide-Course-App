5cf61f8f6fb58ecb3bdd9d8e4b007e2a80230f65
Carlos López <yo@carlop.es>
2021-02-04 10:29:37 +0100

    Understanding Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { CoreModule } from './core.module';
 
 @NgModule({
   declarations: [
@@ -32,12 +28,8 @@ import { ShoppingListModule } from './shopping-list/shopping-list.module';
     RecipesModule,
     ShoppingListModule,
     SharedModule,
+    CoreModule,
   ],
-  providers: [
-      ShoppingListService,
-      RecipeService,
-      {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}
-    ],
   bootstrap: [AppComponent],
 })
 export class AppModule { }
